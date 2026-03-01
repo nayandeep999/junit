b6972c257342c8ae5cd3bded19e290bc76dfef54
Nayandeep <nayandeepsen@gmail.com>
2026-03-01 12:00:00 +0530

    initial commit

---
+++ src/test/java/com/amazon/kodewala/junit_sample/AppTest.java
@@ -0,0 +1,15 @@
+package com.amazon.kodewala.junit_sample;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+
+{
+	@Test
+	public void testDoSomething() {
+
+	}
+}
